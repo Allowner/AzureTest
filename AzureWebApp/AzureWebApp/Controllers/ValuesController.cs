8eaed060d6439a245253dc64e5728c812dd878fc
Vsevolod <vsevolod.gordienko99@gmail.com>
2019-03-25 01:19:34 +0300

    Exception

--- AzureWebApp/AzureWebApp/Controllers/ValuesController.cs
+++ AzureWebApp/AzureWebApp/Controllers/ValuesController.cs
@@ -18,7 +18,7 @@ namespace AzureWebApp.Controllers
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            throw new Exception("exception");
         }
 
         // POST api/values
